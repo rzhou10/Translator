147d04065612beb85d7ee96c4b9e9407199f223a
rzhou10 <rzhou10@gmail.com>
2019-02-17 12:17:34 -0500

    Updates

---
+++ precache-manifest.d1dabafdd01b1ec36b1cc8be02e180eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dbdfbf3e5cb4ca1a625",
+    "url": "/Translator/static/css/main.015c08b0.chunk.css"
+  },
+  {
+    "revision": "9dbdfbf3e5cb4ca1a625",
+    "url": "/Translator/static/js/main.9dbdfbf3.chunk.js"
+  },
+  {
+    "revision": "535a1a5a5098edc2d337",
+    "url": "/Translator/static/js/runtime~main.535a1a5a.js"
+  },
+  {
+    "revision": "9d51dda1dc174f663998",
+    "url": "/Translator/static/js/2.9d51dda1.chunk.js"
+  },
+  {
+    "revision": "09deb37197cd8c37bb0a7b31b9197b93",
+    "url": "/Translator/index.html"
+  }
+];
